b684c9c781d035f9b3114f9624e1718c956a6816
Siddhant <siddhantthawarepatil@gmail.com>
2025-01-30 11:16:31 +0530

    new setup

--- FirebaseStarterSwiftUIApp/scripts/swiftlint-run-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [[ -f "${PODS_ROOT}/SwiftLint/swiftlint" ]]; then
-  "${PODS_ROOT}/SwiftLint/swiftlint"
-else
-  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-fi
